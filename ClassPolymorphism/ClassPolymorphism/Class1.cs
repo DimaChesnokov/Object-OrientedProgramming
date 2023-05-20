a1a2a2709d2845b3f265ce811e04d86320354f47
DimaChesnokov <dc.dima2708@mail.ru>
2023-05-20 11:55:45 +0300

    Простые примеры полиморфизма

---
+++ ClassPolymorphism/ClassPolymorphism/Class1.cs
@@ -0,0 +1,84 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPolymorphism
+{
+    // Базовый класс Фигура
+    public class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Рисую общую фигуру");
+        }
+    }
+
+    // Производный класс Круг
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Рисую круг");
+        }
+    }
+
+    // Производный класс Прямоугольник
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Рисую прямоугольник");
+        }
+    }
+
+    // Производный класс Треугольник
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Рисую треугольник");
+        }
+    }
+
+
+
+
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Неизвестный звук");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Гав-гав!");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Мяу-мяу!");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ ClassPolymorphism/ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassPolymorphism;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            example1();
+        }
+
+        static void example1()
+        {
+
+            Shape[] shapes = new Shape[3];
+            shapes[0] = new Circle();
+            shapes[1] = new Rectangle();
+            shapes[2] = new Triangle();
+
+            foreach (Shape shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+
+
+        static void example2()
+        {
+            Animal animal1 = new Dog();
+            Animal animal2 = new Cat();
+
+            animal1.MakeSound(); // Вывод: Гав-гав!
+            animal2.MakeSound(); // Вывод: Мяу-мяу!
+        }
+    }
+}
