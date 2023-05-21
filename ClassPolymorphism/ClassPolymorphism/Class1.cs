87cfeb7117b0240750720ba3308b16e9039a6622
DimaChesnokov <dc.dima2708@mail.ru>
2023-05-22 00:14:48 +0300

    комментарии к полиморфизмам

--- ClassPolymorphism/ClassPolymorphism/Class1.cs
+++ ClassPolymorphism/ClassPolymorphism/Class1.cs
@@ -7,36 +7,55 @@ using System.Threading.Tasks;
 namespace ClassPolymorphism
 {
     //Полиморфизм с помощью виртуальных методов (Базовый класс)
+
     // Базовый класс Фигура
     public class Shape
     {
+        /// <summary>
+        /// виртуальный метод Draw(), который имеет базовую реализацию для рисования общей фигуры.
+        /// </summary>
         public virtual void Draw()
         {
             Console.WriteLine("Рисую общую фигуру");
         }
     }
 
-    // Производный класс Круг
+    /// <summary>
+    /// Это производный класс от Shape, представляющий круг. 
+    /// </summary>
     public class Circle : Shape
     {
+        /// <summary>
+        /// переопределен метод Draw(), который выводит сообщение "Рисую круг".
+        /// </summary>
         public override void Draw()
         {
             Console.WriteLine("Рисую круг");
         }
     }
 
-    // Производный класс Прямоугольник
+    /// <summary>
+    /// Это производный класс от Shape, представляющий прямоугольник.
+    /// </summary>
     public class Rectangle : Shape
     {
+        /// <summary>
+        /// переопределен метод Draw(), который выводит сообщение "Рисую прямоугольник".
+        /// </summary>
         public override void Draw()
         {
             Console.WriteLine("Рисую прямоугольник");
         }
     }
 
-    // Производный класс Треугольник
+    /// <summary>
+    /// Это производный класс от Shape, представляющий треугольник. 
+    /// </summary>
     public class Triangle : Shape
     {
+        /// <summary>
+        /// переопределен метод Draw(), который выводит сообщение "Рисую треугольник".
+        /// </summary>
         public override void Draw()
         {
             Console.WriteLine("Рисую треугольник");
@@ -46,24 +65,42 @@ namespace ClassPolymorphism
 
 
 
+    /// <summary>
+    /// Это базовый класс, представляющий животное.
+    /// </summary>
     public class Animal
     {
+        /// <summary>
+        ///  виртуальный метод MakeSound(), который имеет базовую реализацию для вывода "Неизвестный звук".
+        /// </summary>
         public virtual void MakeSound()
         {
             Console.WriteLine("Неизвестный звук");
         }
     }
 
+    /// <summary>
+    /// Это производный класс от Animal, представляющий собаку
+    /// </summary>
     public class Dog : Animal
     {
+        /// <summary>
+        /// переопределен метод MakeSound(), который выводит сообщение "Гав-гав!".
+        /// </summary>
         public override void MakeSound()
         {
             Console.WriteLine("Гав-гав!");
         }
     }
 
+    /// <summary>
+    ///  Это производный класс от Animal, представляющий кошку. 
+    /// </summary>
     public class Cat : Animal
     {
+        /// <summary>
+        /// переопределен метод MakeSound(), который выводит сообщение "Мяу-мяу!".
+        /// </summary>
         public override void MakeSound()
         {
             Console.WriteLine("Мяу-мяу!");
@@ -72,13 +109,24 @@ namespace ClassPolymorphism
 
 
     //Полиморфизм с перегрузкой
+    /// <summary>
+    ///  Это класс, в котором определены два статических метода  c одним и тем же именем D, но с различными параметрами.
+    /// </summary>
     public class Z
     {
+        /// <summary>
+        ///  Это метод, принимающий целочисленный параметр k.
+        /// </summary>
+        /// <param name="k"></param>
         public static void D(int k)
         {
             Console.WriteLine("Integer");
         }
 
+        /// <summary>
+        ///  Это метод, принимающий параметр типа double u
+        /// </summary>
+        /// <param name="u"></param>
         public static void D(double u)
         {
             Console.WriteLine("Double");
