00314b903ed4b99807feb4b16213e7290017c497
akkafe1ix <alex.from2018@yandex.ru>
2023-05-21 20:37:32 +0300

    Остались девочки

---
+++ ClassAndRelationship/ClassAndRelationship/Class1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndRelationship
+{
+    /// <summary>
+    /// Обычный класс
+    /// </summary>
+    public class Person
+    {
+        // Поля класса
+        public string Name;
+        public int Age;
+
+        // Конструктор класса
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        // Метод класса
+        public void SayHello()
+        {
+            Console.WriteLine($"Привет, меня зовут {Name}!");
+        }
+    }
+
+
+    /// <summary>
+    /// Абстрактный класс
+    /// </summary>
+    public abstract class Shape
+    {
+        // Абстрактный метод
+        public abstract double CalculateArea();
+
+        // Обычный метод
+        public void DisplayShape()
+        {
+            Console.WriteLine("Это фигура.");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    /// <summary>
+    /// Статический класс
+    /// </summary>
+    public static class MathUtils
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+
+    /// <summary>
+    /// Перечисление
+    /// </summary>
+    public enum DaysOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    /// <summary>
+    /// Вложенные классы
+    /// </summary>
+    public class ShoppingCart
+    {
+        private List<Product> products;
+
+        public ShoppingCart()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void DisplayProducts()
+        {
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Name);
+            }
+        }
+
+        public class Product
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+        }
+    }
+
+
+
+
+
+}
---
+++ ClassAndRelationship/ConsoleTests/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassAndRelationship;
+
+
+namespace ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            example1();
+            example2();
+            example3();
+            example4();
+            example5();
+        }
+
+        public static void example1()
+        {
+            Person person = new Person("Алиса", 25);
+            person.SayHello();  // Выводит: "Привет, меня зовут Алиса!"
+        }
+
+        public static void example2()
+        {
+            Circle circle = new Circle(5);
+            circle.DisplayShape();          // Выводит: "Это фигура."
+            double area = circle.CalculateArea();  // Вычисляет площадь круга
+            Console.WriteLine(area);       // Выводит: 78.53981633974483
+        }
+
+        public static void example3()
+        {
+            int sum = MathUtils.Add(5, 3);  // Вызов статического метода Add
+            Console.WriteLine(sum);        // Выводит: 8
+
+            double product = MathUtils.Multiply(2.5, 4.0);  // Вызов статического метода Multiply
+            Console.WriteLine(product);                    // Выводит: 10.0
+        }
+
+        public static void example4()
+        {
+            DaysOfWeek today = DaysOfWeek.Tuesday;
+            Console.WriteLine(today);  // Выводит: Tuesday
+
+            int dayNumber = (int)today;
+            Console.WriteLine(dayNumber);  // Выводит: 1
+
+            if (today == DaysOfWeek.Saturday || today == DaysOfWeek.Sunday)
+            {
+                Console.WriteLine("Сегодня выходной!");
+            }
+            else
+            {
+                Console.WriteLine("Сегодня рабочий день.");
+            }
+        }
+
+        public static void example5()
+        {
+            ShoppingCart cart = new ShoppingCart();
+
+            ShoppingCart.Product product1 = new ShoppingCart.Product
+            {
+                Name = "Книга",
+                Price = 15.99m
+            };
+            cart.AddProduct(product1);
+
+            ShoppingCart.Product product2 = new ShoppingCart.Product
+            {
+                Name = "Флешка",
+                Price = 9.99m
+            };
+            cart.AddProduct(product2);
+
+            cart.DisplayProducts();
+
+        }
+    }
+}
